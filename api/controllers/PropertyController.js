a02614b37dc27f5ec9a4741b5f433e820a9e08c1
sara <ksara@PIMAGLKSARA8881.users.sphinet.com.sg>
2017-03-21 11:42:21 +0800

    back to old way of doing in sub items list with general comments

--- api/controllers/PropertyController.js
+++ api/controllers/PropertyController.js
@@ -1698,6 +1698,8 @@ module.exports = {
 
 										if(gen_sub_item_id){
 
+											sails.log('found general');
+											sails.log(gen_sub_item_id);
 											
 										    Property_sub_feedback_general.find({item_id: gen_sub_item_id }).exec(function afterwards(err, comments){
 										    	return res.json({status: 1, sub_items: sub_items, gen_comment:comments });
@@ -1705,6 +1707,7 @@ module.exports = {
 									    
 										}
 										else{
+											sails.log('not found general');
 
 											return res.json({status: 1, sub_items: sub_items, gen_comment:'' });
 										}
