f27f8d9bfce65f83db86f130108d1f1693255d25
sara <ksara@PIMAGLKSARA8881.users.sphinet.com.sg>
2017-05-24 15:39:18 +0800

    fixing image upload and resize undefined checked

--- api/controllers/PropertyController.js
+++ api/controllers/PropertyController.js
@@ -843,7 +843,7 @@ module.exports = {
 			 				// 		if (err) console.log(res.json(err) );
 			 				// 	});
 
-									if(req.param('gotImg') != 'undefined' ){
+									if(req.param('gotImg') == 'YES' ){
 
 										var fs = require('fs');
 				            var im = require('imagemagick');
@@ -1654,7 +1654,7 @@ module.exports = {
 								if(user.company_id ==  property_details.company_id ){
 
 
-									if(req.param('gotImg') != 'undefined' ){
+									if(req.param('gotImg') == 'YES' ){
 
 										var fs = require('fs');
 				            var im = require('imagemagick');
