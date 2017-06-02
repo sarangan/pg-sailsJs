f5bb55e5b30a9d0eaf57f921f7f44063454e4edf
sara <ksara@PIMAGLKSARA8881.users.sphinet.com.sg>
2017-06-02 16:55:49 +0800

    fix report generate feeb back object

--- api/controllers/ReportController.js
+++ api/controllers/ReportController.js
@@ -637,13 +637,16 @@ module.exports = {
               photo_date = '';
             }
 
-            //top_photos += '<div style="width: 25%; padding: 10px; background-color: #e1e1e1; display: inline-block; margin: 5px; max-width: 300px;">'+
-              top_photos += '<div class="img-wrapper1">' +
+            top_photos += '<div style="width: 25%; padding: 10px; background-color: #e1e1e1; display: inline-block; margin: 5px; max-width: 300px;">'+
               '<img src="'+ server_image_path +  property_id + '/' + 'report_300_' + (master_item.temp_top_photos[j].file_name.substr(0, master_item.temp_top_photos[j].file_name.lastIndexOf('.')) || master_item.temp_top_photos[j].file_name) + '.jpg'  + '" alt="img" class="rt-2-tbl-img" />'
               '<div style="font-style: italic; color: #a0a0a0;">'+ photo_date +'</div>'+
               '<div>' +
               '<a href="'+ server_image_path +  property_id + '/' + temp_master_items[i].temp_top_photos[j].file_name + '">Ref'+ (j + 1) +'</a>' +
               '</div></div>';
+
+              if(i > 2){
+                break;
+              }
           }
         }
 
@@ -723,6 +726,8 @@ module.exports = {
         if(Object.keys(master_item.feedback_general).length === 0 && master_item.feedback_general.constructor === Object ){
           fgeneral = master_item.feedback_general.comment?master_item.feedback_general.comment:'';
         }
+
+        sails.log(top_photos);
 
         master_html +='<div class="chapter">' +
          '<h1 class="sub-heading">' + master_item.master.name + '</h1>' +
@@ -838,7 +843,8 @@ module.exports = {
 
         }
 
-
+        sails.log(top_photos);
+        
         master_html +='<div class="chapter">' +
          '<h1 class="sub-heading">' + master_item.master.name + '</h1>' +
          '<hr/><div>' +
