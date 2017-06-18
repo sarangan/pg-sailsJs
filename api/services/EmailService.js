bcb13465a829699a98f5dfbd0cdcfe3edec2e555
sara <sarangan12@gmail.com>
2017-06-18 14:49:06 +0800

    auth test new server

--- api/services/EmailService.js
+++ api/services/EmailService.js
@@ -5,17 +5,17 @@ module.exports = {
   sendEmail: function (options, done) {
 
     let transporter = nodemailer.createTransport({
-        host: 'mail.propertyground.com',
+        host: 'propertyground.co.uk',
         port: 465,
         secure: true, // secure:true for port 465, secure:false for port 587
         auth: {
-            user: 'info@propertyground.com',
+            user: 'info@propertyground.co.uk',
             pass: 'shannira123@A'
         }
     });
 
     let mailOptions = {
-        from: '"PropertyGround" <info@propertyground.com>', // sender address
+        from: '"PropertyGround" <info@propertyground.co.uk>', // sender address
         to: options.email, // list of receivers
         subject: options.subject, // Subject line
         text: options.bodytext, // plain text body
