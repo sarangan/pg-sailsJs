38b859c283795638b10b7e162e028fddcdc1d366
sara <ksara@PIMAGLKSARA8881.users.sphinet.com.sg>
2017-06-19 11:34:57 +0800

    test report header

--- api/controllers/ReportController.js
+++ api/controllers/ReportController.js
@@ -1865,8 +1865,8 @@ module.exports = {
           	var options = {
               disableSmartShrinking: true,
               encoding : 'utf-8',
-              marginBottom: '10mm',
-              marginTop: '10mm',
+              // marginBottom: '10mm',
+              // marginTop: '10mm',
               //headerHtml: 'http://52.39.72.94/header.php?address='+ encodeURIComponent(property_info.address_1) +'&type=' + encodeURIComponent(property_info.report_type),
               headerHtml: 'http://propertyground.co.uk/header.php?address=sara&type=assss',
               footerHtml:  'http://propertyground.co.uk/footer.html',
