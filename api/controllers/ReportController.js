25d5b2b3099edb934c9d498cb6baab3eca2621e3
ksara <ksara@mimaglksara8881.users.sphinet.com.sg>
2018-07-05 16:19:32 +0800

    fixing report style 5

--- api/controllers/ReportController.js
+++ api/controllers/ReportController.js
@@ -2136,7 +2136,7 @@ module.exports = {
 
 
                 if(sub_item.photos[l].file_name){
-                  photos_html += '<div style="width: 24%; background-color: #ffffff; display: inline-block; margin: 5px; max-width: 300px;">' +
+                  photos_html += '<div style="width: 22%; background-color: #ffffff; display: inline-block; margin: 5px; max-width: 300px;">' +
                      '<div style="padding: 1px; background-color: #ffffff; border: 1px solid #000000; display: inline-block; ">' +
                      '<img src="' + server_image_path +  property_id + '/' + 'report_300_' + (sub_item.photos[l].file_name.substr(0, sub_item.photos[l].file_name.lastIndexOf('.')) || sub_item.photos[l].file_name) + '.jpg' + '" alt="img" class="rt-2-tbl-img" />' +
                      '</div>' +
@@ -2151,7 +2151,7 @@ module.exports = {
 
          if(desc){
 
-           photos_html += '<div style="width: 24%; background-color: #ffffff; display: inline-block; margin: 5px; max-width: 300px; vertical-align: top;">' +
+           photos_html += '<div style="width: 22%; background-color: #ffffff; display: inline-block; margin: 5px; max-width: 300px; vertical-align: top;">' +
                   desc +
               '</div>';
 
@@ -2373,7 +2373,7 @@ module.exports = {
               }
 
               if(master_item.photos[l].file_name){
-                photos_html += '<div style="width: 20%; background-color: #ffffff; display: inline-block; margin: 5px; max-width: 300px;">' +
+                photos_html += '<div style="width: 22%; background-color: #ffffff; display: inline-block; margin: 5px; max-width: 300px;">' +
                    '<div style="padding: 1px; background-color: #ffffff; border: 1px solid #000000; display: inline-block; ">' +
                    '<img src="' + server_image_path +  property_id + '/' + 'report_300_' + (master_item.photos[l].file_name.substr(0, master_item.photos[l].file_name.lastIndexOf('.')) || master_item.photos[l].file_name) + '.jpg' + '" alt="img" class="rt-2-tbl-img" />' +
                    '</div>' +
