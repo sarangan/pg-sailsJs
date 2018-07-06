5d576b77391a1e54e1eab679e17ea1f14290a18c
ksara <ksara@mimaglksara8881.users.sphinet.com.sg>
2018-07-06 16:42:54 +0800

    testing image crop

--- api/controllers/PropertyController.js
+++ api/controllers/PropertyController.js
@@ -4960,6 +4960,12 @@ module.exports = {
 
 							      fs.createReadStream(_src).pipe(fs.createWriteStream(_dest));
 
+										var temp_src = 'http://propertyground.co.uk/cropper/phpThumb.php?src=' + encodeURIComponent(_src) +'&w=300&h=300&ar=P';;
+
+										fs.createReadStream(_src).pipe(fs.createWriteStream(upload_path + 'x_report_' + path.basename(files[0].fd, path.extname(files[0].fd) ) + '.jpg'));
+
+										sails.log(upload_path + 'x_report_' + path.basename(files[0].fd, path.extname(files[0].fd) ) + '.jpg');
+
 										//resize
 										im.resize({
 											srcPath: _src,
