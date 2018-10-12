87cf8c9c4f278bb6ba19f5e9b4eff0db548bcaf6
ksara <ksara@mimaglksara8881.users.sphinet.com.sg>
2018-10-12 18:00:41 +0800

    bootsrap fixing

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -14,16 +14,17 @@ module.exports.bootstrap = function(cb) {
   var fs = require('fs')
   , path = require('path');
 
-module.exports.bootstrap = function (cb) {
+//module.exports.bootstrap = function (cb) {
   // Whatever else you want to bootstrap...
 
   var postsSource = path.join(process.cwd(), 'assets/images/')
     , postsDest = path.join(process.cwd(), '.tmp/public/images/');
 
   fs.symlink(postsSource, postsDest, function(err) {
+    console.log('bootstrap starting');
     cb(err);
   });
-};
+//};
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
